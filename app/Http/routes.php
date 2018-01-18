36feb3376ab3741c625614707077de7ffb5006ae
Daniel Santoro <daniel.santoro@amplifieragency.com>
2018-01-18 20:43:39 +1100

    Initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use \App\Http\Controllers\Auth\AuthenticateController;
+
+Route::group(['middleware' => ['jwt.auth', 'jwt.refresh']], function () {
+    
+
+    Route::get('/', function () {
+    	return view('welcome');
+	});
+
+});
+
+Route::get('user/auth', 'Auth\AuthenticateController@authenticate');
